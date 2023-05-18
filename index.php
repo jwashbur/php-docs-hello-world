b54bf90772c4a726e62902b004be31cc4b715fe9
Josh Washburn <thewashburns@gmail.com>
2023-05-18 12:56:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-$directory = "/stuff";
+$directory = "/images";
 
 if (is_dir($directory)){
   if ($opendirectory = opendir($directory)){
     while (($file = readdir($opendirectory)) !== false){
-      echo "<a href='https://imagesprocessapp02.azurewebsites.net$directory/" . "$file' target='_blank'>$file</a>"."<br>";
+      echo "<a href='https://imagesprocess02.blob.core.windows.net/container1/" . "$file' target='_blank'>$file</a>"."<br>";
     }
     closedir($opendirectory);
   }
